a5c42079ed78d3803b5d8307ca408465baf984a9
anavasques <ana.vasques@ua.pt>
2014-10-14 16:33:55 +0200

    Improvement disturbance and seed banks

--- firePT.m
+++ firePT.m
@@ -7,32 +7,43 @@ close all
 clear all
 
 %Parameter values
-% ------------------------------------------------------------------------
-
-%Pine
-%-------------------------------------------------------------------------
-% %AgeMP=10;     % Age of maturity pine %start at 6 and regularly 10-15 % in Cronk and Fuller, 1995 [year] 
-% %SeedPP=0.5;   % Seed production pine per occupied cell % should prob increase with age [n/m2/year]
-% SeedLongP=2;  % Seed longevity pine one year % in "Dean et al. (1986)" [year]
-% LSP= 100;     % Life span of pine % in "practices centro pinus" [year]
-% %Seeder
-% %-------------------------------------------------------------------------
-% AgeMS=1;       % Age of maturity seeder % field obs % [year] 
-% SeedPS=500;    % Seed production seeder per occupied cell %invented value% increases with age [n/m2/year]
-% %SeedLongS=200;% Seed longevity seeder %invented value [year]
-% %mSS=XX         % Define mortality of seeds of seeder species
-% LSS=30;        % Life span calluna % in woodland education centre [year]
-% %Oak
-% %-------------------------------------------------------------------------
-% AgeMO=50;       % Age of maturity seeder % Kew % [year] 
-% SeedPO=120;     % Seed production oak per occupied cell isolated tree in Martin?k et al. 2014% [n/m2/year]
-% SeedLongO=1;    %Does not subsist from one year to another %based on literature [year]
-% LSO= 1000;      % Life span quercus robur % in forestar
-
 %%% !!!!!!!!!
 %%%%% INITIALIZE ALL THE PARAMETERS
+%-------------------------------------------------------------------------
+% %%% AgeMP=10;     % Age of maturity pine %start at 6 and regularly 10-15 % in Cronk and Fuller, 1995 [year] 
+% %%%SeedPP=0.5;   % Seed production pine per occupied cell % should prob increase with age [n/m2/year]
+% % SeedLongP=2;  % Seed longevity pine one year % in "Dean et al. (1986)" [year]
+% % LSP= 100;     % Life span of pine % in "practices centro pinus" [year]
+% % %Seeder
+% % %-------------------------------------------------------------------------
+% % AgeMS=1;       % Age of maturity seeder % field obs % [year] 
+% % SeedPS=500;    % Seed production seeder per occupied cell %invented value% increases with age [n/m2/year]
+% % %SeedLongS=200;% Seed longevity seeder %invented value [year]
+% % %mSS=XX         % Define mortality of seeds of seeder species
+% % LSS=30;        % Life span calluna % in woodland education centre [year]
+% % %Oak
+% % %-------------------------------------------------------------------------
+% % AgeMO=50;       % Age of maturity seeder % Kew % [year] 
+% % SeedPO=120;     % Seed production oak per occupied cell isolated tree
+% in Martin?k et al. 2014% [n/m2/year] %% here we assume that the oaks do
+% not become trees unless they become dominant
+% % SeedLongO=1;    %Does not subsist from one year to another %based on literature [year]
+% % LSO= 1000;      % Life span quercus robur % in forestar
 
-mort=1./[LSP,LSS,LS0]; % MORTALITY of Pine, seeder, Oak = 1/lifespan
+SBP1=0;
+SBP2=0; % canopy seeds?? check all
+SB= [  ] % check name and values it is a vector that includes pine, seeder and oak
+SeedFP=0; % new seeds of pine
+SeedFS=0; % new seeds of seeder
+SeedLoss=0; % should be a vector
+SeedFQ=0; % new seeds of oak
+BirdSeedN=0; %annual seed input by birds - oak
+% define and initialize seeds in the canopy of pine
+% define rate litter
+
+
+    
+mort=1./[LSP,LSS,LS0]; % MORTALITY of Pine, Seeder, Oak = 1/lifespan
 AR= [0,0,1]; % ability to resprout: pine=0, seeder=0, oak=1;
 
 %------- Probability of establishment - max and min
@@ -41,37 +52,40 @@ minG=[0.1 0.1 0.1];
 
 %Control constants
 StartTime= 0;                   % [year]
-EndTime= 150;                   % [year] % TO SOLVE: time frame is problematic because the quercus will also become trees and start producing litter
+EndTime= 150;                   % [year]
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%put dt smaller than one year in a way that the probabilities are <1 but not too small otherwise the model runs slowly
+
 dt= 1;                          % [year]
+
 m= 100;                         % [meter]
 
 %Control variables
 %--------------------------------------------------------------------------
 Time = StartTime;
-D=Time/10; % disturbance regime defined with ifs
+%D=Time/10; % disturbance regime defined with ifs
 Pine=0; % will count the number of cells with pine
 Seeder=0; % will count the number of cells with pine
 Oak=0; % will count the number of cells with pine
 
 %%%Initialisation of the matrices 
-%TC - type cover; Age- plant age; SB- Seed Bank); Lit- Litter
+%TC - type of cover: 1- pine; 2- seeder; 3- oak; Age- plant age; SB- Seed Bank); Lit- Litter
 % -------------------------------------------------------------------------
 TC= zeros(m,m);  % Creates a matrix of size m*m filled with zeros       
 Age= zeros(m,m); % Creates a matrix of size m*m filled with zeros    
-SBS= zeros(m,m);  % Creates a matrix of size m*m filled with zeros  
 Lit= zeros(m,m); % Creates a matrix of size m*m filled with zeros
 z= 8;            % Number of neighbours
 
 % Fills the matrix TC with planted pines
 %--------------------------------------------------------------------------
 TC(4:4:96,4:4:96)= 1; % plants 1 pine every 4 meters - prodution stand
-Pine=sum(sum(TC));
-      
+
 %Dynamic
 %--------------------------------------------------------------------------
 while Time < EndTime
     % GERMINATION AND MORTALITY EACH TIME STEP
-    for k=1:round(1/dt) 
+    for k=1:round(1/dt)
         % Creates litter in the neighborhod of pine (8 neighbors)+ the pine site itself
         % ---------------------------------------------------------------------------
         [x,y]=find(TC==1);
@@ -95,7 +109,7 @@ while Time < EndTime
                     % LITTER DEPENDENCE:
                     ProbG(1)=(maxG(1)+minG(1))/2;%(maxG(1)+minG(1))/2+(maxG(1)-minG(1))/2*tanh((Lit(i,j)-2)/amp(1)); % PINE
                     ProbG(2)=(maxG(2)+minG(2))/2+(maxG(2)-minG(2))/2*tanh((2-Lit(i,j))/ampS); % SEEDER ampS=0.3 max=.9 min=0.
-                    ProbG(3)=maxG(3);%(maxG(3)+minG(3))/2+(maxG(3)-minG(3))/2*tanh((Lit(i,j)-2)/amp(3)); % QUERCUS
+                    ProbG(3)=maxG(3)-(maxG(3)-minG(3))*exp(-Lit(i,j)); % QUERCUS
                     
                     ProbG=ProbG*dt;
                     
@@ -104,8 +118,9 @@ while Time < EndTime
                     % ACCOUNT THE SPATIAL OCUPATION AND ALSO THE RESULT OF
                     % THE PROBABILITY IN THE TERM THAT MULTIPLIES WITH
                     % LITTER PROBABILITY (GETS SMALLER) - THINK
+                    ProbS=1.*SB>0;
                     
-                    %ProbG(2)=ProbG*%MULTIPLY BY PROBABILITY DUE TO SEED NUMBER
+                    ProbG=ProbG.*ProbS;%MULTIPLY BY PROBABILITY DUE TO SEED NUMBER
                     
                     if sum(ProbG)*dt>1
                         'sum of probability higher than 1! Please decrease dt'
@@ -113,9 +128,11 @@ while Time < EndTime
                     elseif test<ProbG(1)
                         TC(i,j)=1;
                         Age(i,j)=dt;
+                        
                     elseif test<ProbG(1)+ProbG(2)
                         TC(i,j)=2;
                         Age(i,j)=dt;
+                        
                     elseif test<ProbG(1)+ProbG(2)+ProbG(3)
                         TC(i,j)=3;
                         Age(i,j)=dt;
@@ -133,31 +150,35 @@ while Time < EndTime
     end
     
     % SEED BANK CALCULATION ONLY ONCE A YEAR
-    %include the values for these parameters
+    SB(1)=SBP1+SBP2+SeedFP*sum(sum(TC(Age>AgeMP)==1)); % TWO YEARS OF SEED LIFE
+    SB(1)=SB(1)-SeedLoss(1)*SB(1);
+    SB(2)=SB(2)+SeedFS*(sum(sum(TC==2)))-SeedLoss(2)*SB(2); % LONG SEED LIFE
+    SB(3)=SeedFQ*(sum(sum(TC(Age>AgeMQ)==3)))+randi(BirdSeedN,1); % NO MEMORY
+    SB(3)=SB(3)-SeedLoss(3)*SB(3);
     
-    
-    SB(1)=SBP1+SBP2+SeedFP*sum(sum(TC(Age>AgeMaturityPine)==1));
-    SB(2)=SB(2)+SeedFS*(sum(sum(TC==2)))-SeedLoss*SB(2);
-    SB(3)=SeedFQ*(sum(sum(TC==3)))+randi(BirdSeedN,1);
-    
-    SBP2=SBP1;
-    SBP1=SB(1);
-    
-  
-    %%% DISTURBANCE
-    %if D == (int); % if Time/10 is an integer there is disturbance % interval of 10 years
-    %Lit(i,j)=0;
-    %TC(i,j)= TC(i,j)*AR; % check - the idea is to have 0 for pine and seeder and 1 for oak
-    %Age(i,j)= Age(i,j)*AR; % check - the idea is to have 0 for pine and seeder and 1 for oak
-    %else
-    %    Lit(i,j)= Lit(i-1,j-1)+ 1; % this value should be the factor of accumulation of litter according to real values
-    %   TC(i,j)= TC(i,j);
-    %  Age(i,j)= Age(i-1, j-1)+ dt;
-    %end
-    
-
+    SBP2=SBP1; % PINE SEED BANK OF TWO YEARS BEFORE
+    SBP1=SB(1);% PINE SEED BANK OF 1 YEAR BEFORE
    
-    % % Plot the CA at each time step
+    %accumulation of seeds in the canopy
+    
+    %%% DISTURBANCE
+    D=randi(10,1);
+    if D == 1 % if Time/10 is an integer there is disturbance % interval of 10 years
+        Lit(:,:)=0;
+        for i=1:m
+            for j=1:m
+                TC(i,j)= TC(i,j)*AR(TC(i,j)); % check - the idea is to have 0 for pine and seeder and 1 for oak
+                Age(i,j)= Age(i,j)*AR(TC(i,j)); % check - the idea is to have 0 for pine and seeder and 1 for oak
+            end
+        end
+    end
+    
+    %%% update abundance of different species in the lattice
+    Pine=sum(sum(TC==1));
+    Seeder=sum(sum(TC==2));
+    Oak= sum(sum(TC==3));
+    
+    % % Plot the relative abundance of plant species at each time step
     % %--------------------------------------------------------------------------------------
     % white=[1 1 1];
     % lightgreen=[0.5 1 0.5];
@@ -170,7 +191,7 @@ while Time < EndTime
     % showimage(TC);
     % drawnow;
     
-    
+    % % Plot the relative abundance of plant species over time
     
 end
 
