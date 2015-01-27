36ffd16046ac6e7907a5b5d52ded2bf1d131de1c
anavasques <ana.vasques@ua.pt>
2015-01-27 14:50:49 +0100

    checking and cleaning

--- PlotProbS.m
+++ PlotProbS.m
@@ -9,13 +9,36 @@
 % xlabel('Number of seeds in the seed bank per cell'), ylabel('Probability of establishment')
 % axis([0 1000 0 1])
 
+
+SB=0:1:50;
+% m=100;
+est=7;
+ProbS= 1-(1-1./est).^(SB);
+figure
+plot(SB,ProbS)
+title('pine')
+xlabel('Number of seeds in the seed bank per cell'), ylabel('Probability of establishment')
+axis([0 50 0 1])
+pause
+
+SB=0:1:500;
+% m=100;
+est=100;
+ProbS= 1-(1-1./est).^(SB);
+figure
+plot(SB,ProbS)
+title('seeder' )
+xlabel('Number of seeds in the seed bank per cell'), ylabel('Probability of establishment')
+axis([0 500 0 1])
+pause
+
 SB=0:1:10;
 % m=100;
 est=2;
 ProbS= 1-(1-1./est).^(SB);
-
 figure
 plot(SB,ProbS)
 title('oak' )
 xlabel('Number of seeds in the seed bank per cell'), ylabel('Probability of establishment')
 axis([0 10 0 1])
+pause
