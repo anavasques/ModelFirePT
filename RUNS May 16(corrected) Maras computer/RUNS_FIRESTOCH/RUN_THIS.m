1f1ce3cb9551e8e7bce1911b58a5afaa4bc7856b
anavasques <ana.vasques@ua.pt>
2016-05-18 18:08:10 +0200

    Folder with the scripts for the new runs
    Each subfolder has a file called Runthis. Those are the files that
    should be ran at the same time. Thanks anyway!

---
+++ RUNS May 16(corrected) Maras computer/RUNS_FIRESTOCH/RUN_THIS.m
@@ -0,0 +1,30 @@
+% Partial Run for Mara's computer - linux
+cd ./firePT_F7HS_BIRS_OS
+firePT_F7HS_BIRS_OS
+cd ../firePT_F7HS_BIRS_FS
+firePT_F7HS_BIRS_FS
+cd ../firePT_F7LS_BIRS_OS
+firePT_F7LS_BIRS_OS
+cd ../firePT_F7LS_BIRS_FS
+firePT_F7LS_BIRS_FS
+cd ../firePT_F15HS_BIRS_OS
+firePT_F15HS_BIRS_OS
+cd ../firePT_F15HS_BIRS_FS
+firePT_F15HS_BIRS_FS
+cd ../firePT_F15LS_BIRS_OS
+firePT_F15LS_BIRS_OS
+cd ../firePT_F15LS_BIRS_FS
+firePT_F15LS_BIRS_FS
+cd ../firePT_F30HS_BIRS_OS
+firePT_F30HS_BIRS_OS
+cd ../firePT_F30HS_BIRS_FS
+firePT_F30HS_BIRS_FS
+cd ../firePT_F30LS_BIRS_OS
+firePT_F30LS_BIRS_OS
+cd ../firePT_F30LS_BIRS_FS
+firePT_F30LS_BIRS_FS
+cd ../firePT_F0_BIRS_OS
+firePT_F0_BIRS_OS
+cd ../firePT_F0_BIRS_FS
+firePT_F0_BIRS_FS
+
